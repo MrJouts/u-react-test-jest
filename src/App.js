dd8e7240c8bf19a7ad7b404bf116bcb21002c178
Emiliano <emiliano.hotes@despegar.com>
2022-09-13 10:32:34 -0300

    Code Quiz - Test and code summary form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import { useState } from 'react';
-import './App.css';
+import "./App.css";
+import SummaryForm from "./pages/summary/SummaryForm";
 
 export function replaceCamelWithSpaces(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, ' $1');
-} 
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 
 function App() {
-  const [buttonColor, setButtonColor] = useState('MediumVioletRed');
-  const [disabled, setDisabled] = useState(false);
-
-  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
-
-  return (
-    <div>
-      <button 
-        style={{backgroundColor: disabled ? 'gray' : buttonColor}}
-        disabled={disabled}
-        onClick={() => setButtonColor(() => newButtonColor)}
-      >
-        Change to {replaceCamelWithSpaces(newButtonColor)}
-      </button>
-      <input 
-        type="checkbox"
-        id="disable-check-box"
-        defaultChecked={disabled}
-        onClick={(e) => setDisabled(e.target.checked)} />
-      <label htmlFor="disable-check-box">Disable button</label>
-    </div>
-  );
+  return <SummaryForm />;
 }
 
 export default App;
--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const SummaryForm = () => {
-  return <div>SummaryForm</div>;
+  const [tsChecked, setTsChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: "blue" }}> Terms and Conditions </span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tsChecked}
+          onChange={(e) => setTsChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tsChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
 };
 
 export default SummaryForm;
