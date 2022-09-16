35d54b07cbbc27f27a4cd81c304c0273383ad6a6
Emiliano <emiliano.hotes@despegar.com>
2022-09-16 00:50:37 -0300

    Test and code OrderDetails

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import "./App.css";
-import SummaryForm from "./pages/summary/SummaryForm";
+import Container from "react-bootstrap/Container";
+import OrderEntry from "./pages/entry/OrderEntry";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
 
 export function replaceCamelWithSpaces(colorName) {
   return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
 function App() {
-  return <SummaryForm />;
+  return (
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and wntry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* Confirmation page does not need provider */}
+    </Container>
+  );
 }
 
 export default App;
